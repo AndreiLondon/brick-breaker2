4e70a412204aeaa788b804064e75256988ac9ed3
AndrewLondon <andrewmart@mail.com>
2023-09-10 12:42:53 +0100

    Updated

---
+++ levels/level_2.js
@@ -0,0 +1,87 @@
+export const level2 = [
+
+    { 
+        id: 1,
+        x: 0,
+        y: 0,
+        width: 50,
+        height: 15,
+        color: 'green',
+        type: 'normal'
+     },
+    { 
+        id: 2,
+        x: 100,
+        y: 0,
+        width: 50,
+        height: 15,
+        color: 'green',
+        type: 'normal'
+    },
+    { 
+        id: 3,
+        x: 200,
+        y: 0,
+        width: 50,
+        height: 15,
+        color: 'green',
+        type: 'normal'
+    },   
+    { 
+        id: 4,
+        x: 0,
+        y: 15,
+        width: 50,
+        height: 15,
+        color: 'red',
+        type: 'normal'
+     },
+    { 
+        id: 5,
+        x: 100,
+        y: 15,
+        width: 50,
+        height: 15,
+        color: 'red',
+        type: 'normal'
+    },
+    { 
+        id: 6,
+        x: 200,
+        y: 15,
+        width: 50,
+        height: 15,
+        color: 'red',
+        type: 'normal'
+    },
+   
+    { 
+        id: 7,
+        x: 0,
+        y: 30,
+        width: 50,
+        height: 15,
+        color: 'yellow',
+        type: 'normal'
+     },
+    { 
+        id: 8,
+        x: 100,
+        y: 30,
+        width: 50,
+        height: 15,
+        color: 'yellow',
+        type: 'normal'
+    },
+    { 
+        id: 9,
+        x: 200,
+        y: 30,
+        width: 50,
+        height: 15,
+        color: 'yellow',
+        type: 'normal'
+    }
+    
+   
+]
